314e5a03d61596ba1f998d12d7b8244d6f0c01fe
GattiDev <gattidev323@gmail.com>
2024-11-04 09:31:55 -0300

    GD

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+	<head>
+    	<meta charset="utf-8">
+		<!--/ Para que funcione en los navegadores viejos /-->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<!--/ Para los celulares, para que no hagan zoom /-->
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta author="" date="">
+		<title> : : </title>
+    	
+		<link rel="icon" type="favicon/x-icon" href="materials/favicon.ico" />
+		<link rel="stylesheet" type="text/css" href="css/styles.css" />		
+	</head>
+  	
+	<body>
+			
+		<!-- Head Content - Inicio -->
+		<header>
+			<span> 
+      			<div>
+        			<img src="" alt="" class="">
+      			</div>
+    		</span>
+				
+			<nav class="menu"></nav>		
+		</header> 
+			
+		<!-- Main Content - Inicio -->
+		<main>
+			<div>
+				<section></section>
+
+				<article></article>
+			</div>
+
+			<section>	
+			</section>
+						
+			<div></div>
+		</main> 
+        
+		<!-- Footer - Inicio -->
+		<footer>
+			<div class="container">
+                <p> &copy; Gatti Developer</p>
+                <p> ~ <?php echo date("Y");?> ~ Junín, Buenos Aires. </p>
+			</div>
+		</footer>
+	</body>
+</html>
